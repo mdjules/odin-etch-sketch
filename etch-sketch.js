26572b3e706b98bb8cfe92b60d65d004791cf804
Michael Juliano <mdj.michaeljuliano@gmail.com>
2025-03-30 14:22:42 -0400

    Create dynamic functionality for container and box sizing
    Create variable size that is equal to 100 / number
    of boxes that the user enters. This number is then
    calculated and used to set each box's height and
    width to a percentage so that a grid is always
    formed. The sizing was removed from the css file
    so that the js could dynamically update sizing
    using this methodology.

--- etch-sketch.js
+++ etch-sketch.js
@@ -1,11 +1,37 @@
 
 function createDivs(num) {
-    for (let i = 0; i < num; i++) {
+    //clear the container before creating a new grid
+    let container = document.getElementById("container");
+    container.innerHTML = "";
+
+    let size = 100 / num;
+
+    let containerWidth = window.innerWidth * 0.9;
+    let containerHeight = containerWidth;
+
+    container.style.width = `${containerWidth}px`;
+    container.style.height = `${containerHeight}px`;
+
+    //creates boxes
+    for (let i = 0; i < num * num; i++) {
         let myDivs = document.createElement('div');
         myDivs.classList.add('myDivs');
+        myDivs.style.width = `${size}%`;   // Set each box's width as a percentage of container width
+        myDivs.style.height = `${size}%`;  // Set each box's height as a percentage of container height
         myDivs.textContent = `${i + 1}`;
-        document.getElementById("container").appendChild(myDivs)
+        document.getElementById("container").appendChild(myDivs);
     }
 };
 
-createDivs(256);
+
+let boxesButton = document.querySelector("#boxesButton")
+boxesButton.addEventListener("click", function() {
+   let userInput = prompt("Enter number of rows: ");
+   let num = parseInt(userInput);
+   if (num > 0 && num <= 100) {
+    createDivs(num);
+   } else {
+    alert("Please enter a valid number greater than 0 and less than or equal to 100.")
+   };
+}
+);
